5fbacb3438722bd580cae61165e93cdcb4423693
Chung <nguyentranchung52th@gmail.com>
2025-03-22 17:18:49 +0900

    iconify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,33 +1,11 @@
 import 'vuetify/styles'
-import { createVuetify, type IconSet, type IconProps } from 'vuetify'
-import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import { createVuetify } from 'vuetify'
 import { md3 } from 'vuetify/blueprints'
 import type { VDataTable } from 'vuetify/lib/components/index.mjs'
+import { Icon, type IconProps } from '@iconify/vue'
 export type DataTableHeaders = InstanceType<
   typeof VDataTable
 >['$props']['headers']
-
-function filename(path: string) {
-  return path
-    .split(/(\\|\/)/g)
-    .pop()!
-    .replace(/\.[^/.]+$/, '')
-}
-
-const svgIcons = Object.fromEntries(
-  Object.entries(
-    import.meta.glob('@/assets/icons/*.svg', {
-      eager: true,
-      query: '?raw',
-      import: 'default',
-    }),
-  ).map(([k, v]) => [filename(k), v]),
-)
-
-const custom: IconSet = {
-  component: (props: IconProps) =>
-    h(props.tag, { innerHTML: svgIcons[props.icon as string] }),
-}
 
 const theme = {
   primary: localStorage.getItem('theme-primary') || '#1697f6',
@@ -69,9 +47,59 @@ export default createVuetify({
     },
   },
   icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: { mdi, custom },
+    defaultSet: 'iconify',
+    aliases: {
+      collapse: 'mdi-chevron-up',
+      complete: 'mdi-check',
+      cancel: 'mdi-close-circle',
+      close: 'mdi-close',
+      delete: 'mdi-close-circle',
+      // delete (e.g. v-chip close)
+      clear: 'mdi-close-circle',
+      success: 'mdi-check-circle',
+      info: 'mdi-information',
+      warning: 'mdi-alert-circle',
+      error: 'mdi-close-circle',
+      prev: 'mdi-chevron-left',
+      next: 'mdi-chevron-right',
+      checkboxOn: 'mdi-checkbox-marked',
+      checkboxOff: 'mdi-checkbox-blank-outline',
+      checkboxIndeterminate: 'mdi-minus-box',
+      delimiter: 'mdi-circle',
+      // for carousel
+      sortAsc: 'mdi-arrow-up',
+      sortDesc: 'mdi-arrow-down',
+      expand: 'mdi-chevron-down',
+      menu: 'mdi-menu',
+      subgroup: 'mdi-menu-down',
+      dropdown: 'mdi-menu-down',
+      radioOn: 'mdi-radiobox-marked',
+      radioOff: 'mdi-radiobox-blank',
+      edit: 'tabler:pencil',
+      ratingEmpty: 'mdi-star-outline',
+      ratingFull: 'mdi-star',
+      ratingHalf: 'mdi-star-half-full',
+      loading: 'mdi-cached',
+      first: 'mdi-page-first',
+      last: 'mdi-page-last',
+      unfold: 'mdi-unfold-more-horizontal',
+      file: 'mdi-paperclip',
+      plus: 'mdi-plus',
+      minus: 'mdi-minus',
+      calendar: 'mdi-calendar',
+      eyeDropper: 'mdi-eyedropper',
+      // custom
+      search: 'mdi-magnify',
+      actionDelete: 'tabler:trash',
+    },
+    sets: {
+      iconify: {
+        // @ts-expect-error Parameter props implicitly has an any type.
+        component: (props: IconProps & { size?: null | string | number }) => {
+          return h<IconProps>(Icon, props)
+        },
+      },
+    },
   },
   display: {
     mobileBreakpoint: 'sm',
--- vite.config.ts
+++ vite.config.ts
@@ -7,36 +7,10 @@ import Components from 'unplugin-vue-components/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import Layouts from 'vite-plugin-vue-meta-layouts'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
-import regexpPlugin from 'rollup-plugin-regexp'
-import * as mdicons from '@mdi/js'
-
-const mdi: Record<string, string> = {}
-Object.keys(mdicons).forEach((key) => {
-  const value = (mdicons as Record<string, string>)[key]
-  mdi[
-    key
-      .replace(/([A-Z])/g, '-$1')
-      .toLowerCase()
-      .replace(/([0-9]+)/g, '-$1')
-  ] = value
-})
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    regexpPlugin({
-      exclude: ['node_modules/**'],
-      find: /\b(?<![/\w])(mdi-[\w-]+)\b(?!\.)/,
-      replace: (match: string) => {
-        if (mdi[match]) {
-          return mdi[match]
-        } else {
-          console.warn('[plugin-regexp] No matched svg icon for ' + match)
-          return match
-        }
-      },
-      sourcemap: false,
-    }),
     VueRouter({ importMode: 'sync', dts: './src/typed-router.d.ts' }),
     Vue({
       template: { transformAssetUrls },
