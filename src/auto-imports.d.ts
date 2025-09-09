28e2dced5b2a0a2a62919143f187ee9fff771043
Chung <nguyentranchung52th@gmail.com>
2025-09-09 10:26:01 +0900

    Update auto-imports.d.ts

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -15,8 +15,6 @@ declare global {
   const customRef: typeof import('vue')['customRef']
   const defineAsyncComponent: typeof import('vue')['defineAsyncComponent']
   const defineComponent: typeof import('vue')['defineComponent']
-  const defineLoader: typeof import('vue-router/auto')['defineLoader']
-  const definePage: typeof import('unplugin-vue-router/runtime')['definePage']
   const defineStore: typeof import('pinia')['defineStore']
   const effectScope: typeof import('vue')['effectScope']
   const getActivePinia: typeof import('pinia')['getActivePinia']
